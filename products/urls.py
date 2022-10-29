7a41b518bb2843243ca718fd94418dca39c39ac9
AnaAn01 <anastasia.anto01@gmail.com>
2022-10-30 00:54:14 +0300

    with admin items and new urls

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,17 @@
 from django.urls import path
-from . import views
+from .views import (
+    get_data,
+    remove_from_cart,
+    add_to_cart,
+    # ProductView,
+    CatalogView
+)
 
 app_name = 'products'
 urlpatterns = [
-    path('', views.catalog, name='catalog')
+    path('', CatalogView.as_view(), name='catalog'),
+    # path('product/<pk>/', ProductView.as_view(), name='product'),
+    path('add-to-cart/<pk>/', add_to_cart, name='add-to-cart'),
+    path('remove-from-cart/<pk>/', remove_from_cart, name='remove-from-cart'),
+    path('get_data/', get_data, name='get_data'),
 ]
